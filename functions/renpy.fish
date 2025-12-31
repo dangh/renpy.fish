5eaab6549d09d5d2fcf472b96b5b09f942080f9d
Dang <huynhminhdang@gmail.com>
2025-12-31 14:13:09 +0700

    Refactor renpy functions for better organization

--- functions/renpy.fish
+++ functions/renpy.fish
@@ -1,10 +1,12 @@
-function renpy_index_update
+set -g renpy_data $HOME/.local/share/renpy
+
+function _renpy_index_update
     argparse -i f/force -- $argv
 
-    set -l data $HOME/.local/share/renpy && command mkdir -p $data
-    set -l index $data/.index
+    command mkdir -p $renpy_data
+    set -l index $renpy_data/.index
     set -l today (date "+%Y-%m-%d")
-    mkdir -p "$data"
+    mkdir -p "$renpy_data"
 
     set -l outdated 0
     if not test -f "$index" -o (date -r "$index" "+%Y-%m-%d") != "$today"
@@ -14,52 +16,48 @@ function renpy_index_update
     if test "$outdated" -eq 1; or set -q _flag_force
         curl -s https://www.renpy.org/dl/ \
             | string match -rag '>(\d+\.\d+\.\d+)/<' \
-            | sort -ruV >"$index"
+            | sort -uV >"$index"
     end
 end
 
-function renpy_version_resolve -a v
-    renpy_index_update
-
-    set -l index $HOME/.local/share/renpy/.index
-
-    switch "$v"
-        case ""
-            head -n1 $index
-        case "*.*.*"
-            string match "$v" <$index | head -n1
-        case "*.*"
-            string match "$v.*" <$index | head -n1
-        case "*"
-            string match "$v.*.*" <$index | head -n1
-    end
+function _renpy_version_match -a v
+    echo $v \
+        | string replace -r -- '^v?(\d+|\d+\.\d+)$' '$1.' \
+        | string escape --style regex \
+        | read -l regex
+    echo "^$regex"
 end
 
-function renpy_build -a v
-    set -l builds $HOME/.local/share/renpy/builds && command mkdir -p $builds
-    ls $builds \
-        | string match -rag 'renpy-(\d+\.\d+\.\d+)\.tar.gz' \
-        | sort -ruV \
-        | while read -l V
-        switch "$v"
-            case ""
-                echo $V
-            case "*.*.*"
-                string match "$v" $V
-            case "*.*"
-                string match "$v.*" $V
-            case "*"
-                string match "$v.*.*" $V
-        end
-    end | head -n1 | read -l V
+function _renpy_version_list
+    for f in $renpy_data/renpy-*.tar.gz
+        echo $f \
+            | string replace $renpy_data/renpy- '' \
+            | string replace .tar.gz ''
+    end | sort -uV
+end
+
+function _renpy_build -a v
+    test -n "$v" || begin
+        echo "Version required" >&2
+        return 1
+    end
+
+    _renpy_version_list | sort -ruV | string match -re (_renpy_version_match $v) | read -l V
+
+    if test -n "$V"
+        echo Found renpy-$V locally
+        return
+    end
 
     if test -z "$V"
-        renpy_version_resolve "$v" | read V
+        _renpy_index_update
+
+        cat $renpy_data/.index | sort -ruV | string match -re (_renpy_version_match $v) | read -l V
 
         echo Downloading SDK v$V
         set -l build_dir (mktemp -d)
         curl --progress-bar -L "https://renpy.org/dl/$V/renpy-$V-sdk.tar.bz2" | tar -xj -C "$build_dir" --strip-components 1 || begin
-            echo "Error: Failed to download SDK" >&2
+            echo Failed to download SDK >&2
             return 1
         end
 
@@ -72,15 +70,39 @@ function renpy_build -a v
 
         echo Codesigning renpy-$V.app
         codesign --force --deep --sign RenPy "$build_dir/renpy.app" 2>/dev/null; or begin
-            echo "Warning: Codesigning failed"
+            echo Failed to codesign app >&2
             return 1
         end
 
-        echo Archiving $builds/renpy-$V.app
-        tar -czf "$builds/renpy-$V.tar.gz" -C $build_dir renpy.app
+        echo Archiving renpy-$V.app
+        tar -czf "$renpy_data/renpy-$V.tar.gz" -C $build_dir renpy.app
     end
 end
 
+function _renpy_use -a v
+    if test -z "$v" -a -d renpy.app
+        if test (count renpy.app/Contents/Resources/lib/python3.*) -gt 0
+            set v 8
+        else if test (count renpy.app/Contents/Resources/lib/python2.*) -gt 0
+            set v 7
+        end
+    end
+    _renpy_build $v
+    _renpy_version_list | sort -ruV | string match -re (_renpy_version_match $v) | read -l V
+
+    set -l tmp_dir (command mktemp -d)
+    command tar -xzf $renpy_data/renpy-$V.tar.gz -C "$tmp_dir"
+    command rsync -a --delete "$tmp_dir/renpy.app" ./
+    rm -rf $tmp_dir
+end
+
 function renpy -a cmd
-    renpy_$cmd $argv[2..]
+    switch "$cmd"
+        case list
+            _renpy_version_list
+        case build
+            _renpy_build $argv[2..]
+        case use
+            _renpy_use $argv[2..]
+    end
 end
